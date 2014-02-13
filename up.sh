ebc988f7f378dcdefeee3397eff446a4b11dbee5
Brian Moss <bmoss@redhat.com>
2014-02-13 10:55:31 +1000

    Update up.sh to svn update intelligently in the CloudForms directory

--- up.sh
+++ up.sh
@@ -2,5 +2,13 @@
 
 bash /home/bmoss/scripts/bash/testdate.sh
 
-for dir in ~/CloudForms/*; do (cd "$dir" && echo 'CloudForms' && svn up && echo); done
+for dir in ~/CloudForms/*; do
+	if test -d "$dir"; then
+		cd "$dir"
+		if test -e .svn; then
+			echo "$dir" && svn up && echo
+		fi
+	fi
+done
+
 for dir in ~/csp/cloudforms/*; do (cd "$dir" && csprocessor pull); done
