44febc0364d75ff7bd42f3ab292b81ef78cf6ca4
Brian Moss <bmoss@redhat.com>
2014-02-20 10:00:55 +1000

    Update pullall.sh with new up.sh script

--- pullall.sh
+++ pullall.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-#run kinit if required
-bash /home/bmoss/scripts/bash/testdate.sh
-
-#csprocessor repos
-bash /home/bmoss/scripts/bash/pullcsp.sh
+#run kinit if required and update svn and csp repos
+bash /home/bmoss/scripts/bash/up.sh
 
 #git repos
 bash /home/bmoss/scripts/bash/pullos.sh
 bash /home/bmoss/scripts/bash/mygit.sh
-
-#svn repos
-for dir in ~/CloudForms/*; do (cd "$dir" && echo 'CloudForms' && svn up && echo); done
