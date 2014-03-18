7f365ee575a941d496ea48614e6940d3868f064f
Brian Moss <bmoss@redhat.com>
2014-03-19 09:11:16 +1000

    Update room.sh to backup The_Room.txt

--- room.sh
+++ room.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 file="/home/bmoss/Dropbox/The_Room.txt"
+backup="/home/bmoss/Documents/personal/The_Room.txt"
 if [ "$1" = "" ]; then
 	gnome-open $file
 else
@@ -8,4 +9,8 @@ else
 	echo $1 >> "$file"
 	new=$(tail -1 "$file")
 	test "$new" = "$1" ; echo "Confirmed"
+	echo >> "$backup"
+	echo $1 >> "$backup"
+	new=$(tail -1 "$backup")
+	test "$new" = "$1" ; echo "Backup Confirmed"
 fi
