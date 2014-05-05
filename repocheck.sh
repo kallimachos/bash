fe3ead3895efc552c7ae7e93aaa42686d16f9c2d
kallimachos <kallimachos@gmail.com>
2014-05-05 12:36:19 +1000

    update repocheck.sh

--- repocheck.sh
+++ repocheck.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
 
-echo
-loc=$(pwd)
-cd ~/OpenShift/administration-guide
-echo -e "\e[1;35mAdministration Guide\e[00m"
-git status -s && echo
-cd ../architecture-guide
-echo -e "\e[1;35mArchitecture Guide\e[00m"
-git status -s && echo
-cd ../deployment-guide
-echo -e "\e[1;35mDeployment Guide\e[00m"
-git status -s && echo
-cd ../getting-started-guide
-echo -e "\e[1;35mGetting Started Guide\e[00m"
-git status -s && echo
-cd ../release-notes
-echo -e "\e[1;35mRelease Notes\e[00m"
-git status -s && echo
-cd ../rest-api-guide
-echo -e "\e[1;35mREST API Guide\e[00m"
-git status -s && echo
-cd ../tech_notes
-echo -e "\e[1;35mTechnical Notes\e[00m"
-git status -s && echo
-cd ../troubleshooting-guide
-echo -e "\e[1;35mTroubleshooting Guide\e[00m"
-git status -s && echo
-cd ../user-guide
-echo -e "\e[1;35mUser Guide\e[00m"
-git status -s && echo
-cd $loc
+for dir in ~/code/python/*; do
+	cd "$dir"
+	if test -e .git; then
+		echo $dir
+		git status -s && echo
+	fi
+done
+
+for dir in ~/scripts/*; do
+	cd "$dir"
+	if test -e .git; then
+		echo $dir
+		git status -s && echo
+	fi
+done
