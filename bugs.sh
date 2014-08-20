681c85abf5209fd5891dc82f51821420cf45b745
kallimachos <kallimachos@gmail.com>
2014-08-20 15:06:59 +1000

    Update bug query list

--- bugs.sh
+++ bugs.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-declare -A queries=( ["1"]="myCloudForms" ["2"]="Suyog" ["3"]="Shikha" ["4"]="ON_QA" ["5"]="Errata 5.2.z" ["6"]="Errata 5.3.0" ["7"]="CloudFormsDocs" )
-
+declare -A queries=( ["1"]="myCloudForms" ["2"]="Suyog" ["3"]="Shikha" ["4"]="ON_QA" ["5"]="CloudFormsDocs" ["6"]="Errata 5.2.z" ["7"]="Errata 5.3.0" )
 echo
 for item in "${!queries[@]}"; do echo "$item - ${queries["$item"]}"; done
 echo
