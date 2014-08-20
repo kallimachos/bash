f2b2f86bbb98388adacf82f1ef84341c47f77b01
kallimachos <kallimachos@gmail.com>
2014-08-20 15:31:37 +1000

    Add Need_Info to bugs.sh

--- bugs.sh
+++ bugs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -A queries=( ["1"]="myCloudForms" ["2"]="Suyog" ["3"]="Shikha" ["4"]="ON_QA" ["5"]="CloudFormsDocs" ["6"]="Errata 5.2.z" ["7"]="Errata 5.3.0" )
+declare -A queries=( ["1"]="myCloudForms" ["2"]="Suyog" ["3"]="Shikha" ["4"]="Need_Info" ["5"]="ON_QA" ["6"]="CloudFormsDocs" ["7"]="Errata 5.2.z" ["8"]="Errata 5.3.0" )
 echo
 for item in "${!queries[@]}"; do echo "$item - ${queries["$item"]}"; done
 echo
