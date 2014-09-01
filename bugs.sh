6270047a4e0dd58b7b2c5fbab00c47abd2fccc18
kallimachos <kallimachos@gmail.com>
2014-09-02 09:43:41 +1000

    Add more searches to bugs.sh

--- bugs.sh
+++ bugs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-declare -A queries=( ["1"]="myCloudForms" ["2"]="Suyog" ["3"]="Shikha" ["4"]="Need_Info" ["5"]="ON_QA" ["6"]="CloudFormsDocs" ["7"]="Errata 5.2.z" ["8"]="Errata 5.3.0" )
+declare -A queries=( ["1"]="myCloudForms" ["2"]="Suyog" ["3"]="Shikha" ["4"]="Need_Info" ["5"]="ON_QA" ["6"]="CloudFormsDocs" ["7"]="Doc Bugs in Errata" ["8"]="Errata 5.2.z" ["9"]="Errata 5.3.0" ["10"]="RHCI")
 echo
 for item in "${!queries[@]}"; do echo "$item - ${queries["$item"]}"; done
 echo
 echo -n "Select search: "
 read search && echo
-bugzilla query --savedsearch="${queries[$search]}" | tee /dev/tty | wc -l  && echo
+bugzilla query --savedsearch="${queries[$search]}" | tee /dev/tty | wc -l && echo
