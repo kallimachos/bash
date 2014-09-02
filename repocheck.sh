88d5c9989e06b68d902a05da5068b1fe6d2bf49c
kallimachos <kallimachos@gmail.com>
2014-09-02 21:17:36 +1000

    Add ~/code/* to repocheck

--- repocheck.sh
+++ repocheck.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+for dir in ~/code/*; do
+        cd "$dir"
+        if test -e .git; then
+                echo $dir
+                git status -s
+        fi
+done
+
 for dir in ~/code/python/*; do
 	cd "$dir"
 	if test -e .git; then
