5434dc29202b5d6d005a496f42bbf4c612042271
kallimachos <kallimachos@gmail.com>
2015-01-09 10:46:44 +1000

    Add usage comment to cleanscreen.txt

--- cleanscreen.sh
+++ cleanscreen.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# removes whitespace between <screen> tags and swaps invalid characters for valid XML codes
+
 sed -i ':a;N;$!ba;s/[ \t]*<screen>\n/<screen>/g' $1
 sed -i "s/\`/'/g" $1
 sed -i 's/C\&U/C\&amp\;U/g' $1
