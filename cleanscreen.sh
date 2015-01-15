5d7c1ec4bcef0e564a938d5e8e971715555626eb
kallimachos <kallimachos@gmail.com>
2015-01-15 16:57:55 +1000

    update cleanscreen

--- cleanscreen.sh
+++ cleanscreen.sh
@@ -3,6 +3,7 @@
 # removes whitespace between <screen> tags and swaps invalid characters for valid XML codes
 
 sed -i ':a;N;$!ba;s/[ \t]*<screen>\n/<screen>/g' $1
+sed -i ':a;N;$!ba;s/[ \t]*<screen>\t/<screen>/g' $1
 sed -i "s/\`/'/g" $1
 sed -i 's/C\&U/C\&amp\;U/g' $1
 sed -i 's/ \& / and /g' $1
