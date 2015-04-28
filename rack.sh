5f065db588db43fb95d91a6e06563420af7fa284
kallimachos <kallimachos@gmail.com>
2015-04-28 18:50:16 +1000

    Update rack.sh

--- rack.sh
+++ rack.sh
@@ -2,6 +2,9 @@
 
 for dir in ~/rackspace/*; do
 	if test -d $dir && test -e $dir/.git; then
-        cd $dir && git pull
+        cd $dir
+        git fetch upstream
+        git merge upstream/master
+        git push origin master
 	fi
 done
