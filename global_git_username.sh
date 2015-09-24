3a3956ea5e69c7f430deb3e1e2b11e82e99185a3
kallimachos <kallimachos@gmail.com>
2015-09-24 10:41:49 +1000

    Add git username scripts

---
+++ global_git_username.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for dir in ~/openstack/*; do
+	if test -d $dir && test -e $dir/.git; then
+        cd $dir
+        git config -l | grep user.name
+        echo
+	fi
+done
+
+for dir in ~/rpcdocs/*; do
+	if test -d $dir && test -e $dir/.git; then
+        cd $dir
+        git config -l | grep user.name
+        echo
+	fi
+done
---
+++ local_git_username.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+for dir in ~/code/*; do
+	if test -d $dir && test -e $dir/.git; then
+        cd $dir
+        git config --local user.name kallimachos
+        git config -l | grep user.name
+        echo
+	fi
+done
+
+for dir in ~/code/python/*; do
+	if test -d $dir && test -e $dir/.git; then
+        cd $dir
+        git config --local user.name kallimachos
+        git config -l | grep user.name
+        echo
+	fi
+done
+
+for dir in ~/scripts/*; do
+	if test -d $dir && test -e $dir/.git; then
+        cd $dir
+        git config --local user.name kallimachos
+        git config -l | grep user.name
+        echo
+	fi
+done
