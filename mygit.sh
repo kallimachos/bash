9b202554f4f7a69d2e928ed162c6828e2709fe82
kallimachos <kallimachos@gmail.com>
2015-09-25 16:12:00 +1000

    updates to git scripts

--- mygit.sh
+++ mygit.sh
@@ -2,18 +2,18 @@
 
 for dir in ~/code/*; do
 	if test -d $dir && test -e $dir/.git; then
-        cd $dir && git pull
+        cd $dir && echo $dir && git pull
 	fi
 done
 
 for dir in ~/code/python/*; do
 	if test -d $dir && test -e $dir/.git; then
-        cd $dir && git pull
+        cd $dir && echo $dir && git pull
 	fi
 done
 
 for dir in ~/scripts/*; do
 	if test -d $dir && test -e $dir/.git; then
-        cd $dir && git pull
+        cd $dir && echo $dir && git pull
 	fi
 done
