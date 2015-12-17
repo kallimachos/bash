2a70773555cdd3e32844c775a0f5c72839baf6f0
kallimachos <kallimachos@gmail.com>
2015-12-17 17:11:24 +1000

    Fix bashate warning

--- backup.sh
+++ backup.sh
@@ -4,16 +4,18 @@
 
 if [ "$1" = "all" ]; then
     sudo rsync -azvACHS --delete \
-    --progress --exclude={"/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/",\
-    "/media/","/lost+found/"} /* \
+    --progress --exclude={"/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/"} \
+    --exclude={"/media/","/lost+found/"} /* \
     /run/media/bmoss/FreeAgent\ GoFlex\ Drive/FedoraBackup/
 else
-    rsync -azvACHS --delete \
-    --progress --exclude={"/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/",\
-    "/media/","/lost+found/",".gem",".ICEauthority",".macromedia",".pki",\
-    ".shutter",".gimp-2.8",".java",".mozilla",".python_history",".adobe",\
-    ".cache",".dropbox",".gnome2",".gnome2_private",".novaclient",\
-    ".thumbnails",".bash_history",".dropbox-dist",".gnupg",".tox",\
-    ".bash_logout",".esd_auth",".gphoto",".m2"} /home/bmoss/ \
+    rsync -nazvACHS --delete \
+    --progress --exclude={"/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/"} \
+    --exclude={"/media/","/lost+found/",".gem/",".ICEauthority/"} \
+    --exclude={".macromedia/",".pki/",".shutter/",".gimp-2.8/",".java/"} \
+    --exclude={".mozilla/",".python_history/",".adobe/",".cache/"} \
+    --exclude={".dropbox/",".gnome2/",".gnome2_private/",".novaclient/"} \
+    --exclude={".thumbnails/",".bash_history/",".dropbox-dist/",".gnupg/"} \
+    --exclude={".tox/",".bash_logout/",".esd_auth/",".gphoto/",".m2/"} \
+    /home/bmoss/ \
     /run/media/bmoss/FreeAgent\ GoFlex\ Drive/FedoraBackup/home/bmoss/
 fi
