cd4fc3b40651021fb1dd7ffbda91c65bc908dcf6
kallimachos <kallimachos@gmail.com>
2015-12-17 16:55:27 +1000

    update backup.sh

--- backup.sh
+++ backup.sh
@@ -10,6 +10,10 @@ if [ "$1" = "all" ]; then
 else
     rsync -azvACHS --delete \
     --progress --exclude={"/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/",\
-    "/media/","/lost+found/"} /home/bmoss/ \
+    "/media/","/lost+found/",".gem",".ICEauthority",".macromedia",".pki",\
+    ".shutter",".gimp-2.8",".java",".mozilla",".python_history",".adobe",\
+    ".cache",".dropbox",".gnome2",".gnome2_private",".novaclient",\
+    ".thumbnails",".bash_history",".dropbox-dist",".gnupg",".tox",\
+    ".bash_logout",".esd_auth",".gphoto",".m2"} /home/bmoss/ \
     /run/media/bmoss/FreeAgent\ GoFlex\ Drive/FedoraBackup/home/bmoss/
 fi
