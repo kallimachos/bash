cdc102c2a827f5495419deb8d9b64461e57b18cb
kallimachos <kallimachos@gmail.com>
2016-01-29 09:52:22 +1000

    Add y flag to rackup.sh for internal repo

--- rackup.sh
+++ rackup.sh
@@ -2,9 +2,20 @@
 
 for dir in ~/rpcdocs/*; do
     if test -d $dir && test -e $dir/.git; then
-        cd $dir
-        git fetch upstream
-        git merge upstream/master
-        git push origin master
+        if [ "$1" == "y" ]; then
+            cd $dir
+            git fetch upstream
+            git merge upstream/master
+            git push origin master
+        else
+            cd $dir
+            if [ $dir == ~/rpcdocs/docs-internal ]; then
+                true
+            else
+                git fetch upstream
+                git merge upstream/master
+                git push origin master
+            fi
+        fi
     fi
 done
