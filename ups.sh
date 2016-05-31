6048f980334427574c130cb0b545a26ff6a6db8e
kallimachos <kallimachos@gmail.com>
2016-05-31 19:45:39 +1000

    Fix bashate warnings

--- ups.sh
+++ ups.sh
@@ -3,9 +3,9 @@
 # Updates Fedora and pip packages.
 
 if [[ uname == 'linux-gnu' ]]; then
-   echo 'sudo dnf update -y'
-   sudo dnf update -y
-   echo
+    echo 'sudo dnf update -y'
+    sudo dnf update -y
+    echo
 fi
 
 echo 'sudo pip-review --auto'
