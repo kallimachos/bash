9b3b912e1e5485fc2353539b8bd5766075a22ba7
kallimachos <kallimachos@gmail.com>
2016-08-17 15:41:39 +1000

    bump try version

--- try.sh
+++ try.sh
@@ -10,10 +10,10 @@ rm -rf 34-sdist 34-bdist
 python3 setup.py sdist bdist_wheel
 
 virtualenv 34-sdist
-34-sdist/bin/pip install dist/sphinxmark-0.1.0.tar.gz
+34-sdist/bin/pip install dist/sphinxmark-0.1.2.tar.gz
 
 virtualenv 34-bdist
-34-bdist/bin/pip install dist/sphinxmark-0.1.0-py2.py3-none-any.whl
+34-bdist/bin/pip install dist/sphinxmark-0.1.2-py2.py3-none-any.whl
 
 rm -rf build sphinxmark.egg-info
 
