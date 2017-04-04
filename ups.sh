47ec4b8df0d4b704a9b4713c18145812daa692ac
kallimachos <kallimachos@gmail.com>
2017-04-05 09:12:25 +1000

    echo freshclam

--- ups.sh
+++ ups.sh
@@ -2,8 +2,9 @@
 
 # Updates Fedora and pip packages.
 if [[ `uname` == 'Linux' ]]; then
-    echo 'sudo dnf update -y'
+    echo 'sudo freshclam'
     sudo freshclam
+    echo 'sudo dnf update -y'
     sudo dnf update -y
     echo
 fi
