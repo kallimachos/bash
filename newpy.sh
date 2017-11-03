cdd21c8ab061ba075aec9491519fbbd6775748b5
kallimachos <kallimachos@gmail.com>
2017-11-03 19:01:40 +1100

    update newpy

--- newpy.sh
+++ newpy.sh
@@ -2,78 +2,119 @@
 
 # Initializes a new git repository for a python project.
 
-repo_name=$1
+abort() {
+  printf "\033[31mError: $@\033[0m\n" && exit 1
+}
 
-dir=$(pwd)
-dir_name=`basename $(pwd)`
+display_help() {
+  cat <<-help
 
-if [ "$repo_name" = "" ]; then
+  Usage: newpy [options]
+
+  Commands:
+
+    newpy           Start interactive Python repository creator
+
+  Options:
+
+    -h, --help      Display help information
+
+help
+  exit 0
+}
+
+repo_name() {
+    dir_name=`basename $(pwd)`
+
     echo -n "Repo name (hit enter to use '$dir_name')? "
     read repo_name
-fi
 
-if [ "$repo_name" = "" ]; then
-    repo_name=$dir_name
-fi
+    if [ "$repo_name" = "" ]; then
+        repo_name=$dir_name
+    fi
 
-username=`git config github.username`
-if [ "$username" = "" ]; then
-    echo "Could not find username."
-    echo "Run 'git config --global github.username <username>'"
-    invalid_credentials=1
-fi
+    if [ "$repo_name" != $dir_name ]; then
+        mkdir -p $repo_name
+        cd $repo_name
+        repo_dir=$(pwd)
+    fi
+}
 
-token=`git config github.token`
-if [ "$token" = "" ]; then
-    echo "Could not find token, run 'git config --global github.token <token>'"
-    invalid_credentials=1
-fi
+git_config() {
+    username=`git config github.username`
+    if [ "$username" = "" ]; then
+        abort "Could not find username.\nRun 'git config --global github.username <username>'"
+    fi
 
-if [ "$invalid_credentials" == "1" ]; then
-    return 1
-fi
+    token=`git config github.token`
+    if [ "$token" = "" ]; then
+        abort "Could not find token, run 'git config --global github.token <token>'"
+    fi
+}
 
-echo -n "Use python project template? (y/n) "
-read template
-if [ "$template" = "y" ]; then
-    template=~/code/python/template
-    cd $template && git clean -xfd
-    rsync -a --exclude='MANIFEST' --exclude='.git/' ./ $dir
-    cd $dir
-else
-    echo "No template selected."
-    echo "$repo_name" > README.rst
-fi
+template() {
+    echo -n "Use python project template? (y/n) "
+    read template
+    if [ "$template" = "y" ]; then
+        template=~/code/python/template
+        cd $template && git clean -xfd
+        rsync -a --exclude='MANIFEST' --exclude='.git/' ./ $repo_dir
+        cd $repo_dir
+        sed -i "" "s/template/$repo_name/g" README.rst setup.py tox.ini template/__init__.py template/template.py tests/test.py tools/deploy.sh doc/index.rst doc/conf.py
+        mv template/template.py template/$repo_name.py
+        mv template $repo_name
+    else
+        echo "No template selected."
+        echo "$repo_name" > README.rst
+    fi
+}
 
-echo -n "Initialising local repository ..."
-git init > /dev/null 2>&1
-git config --local user.name kallimachos
-git add .
-git commit -q -a -m "Initial commit"
-echo " done."
-
-echo -n "Creating Github repository '$repo_name' ..."
-curl -u "$username:$token" https://api.github.com/user/repos -d \
-'{"name":"'$repo_name'"}' > /dev/null 2>&1
-echo " done."
-
-echo -n "Pushing local code to remote ..."
-git remote add origin git@github.com:$username/$repo_name.git > /dev/null 2>&1
-git push -u origin master > /dev/null 2>&1
-echo " done."
-echo -n "Create gh-page? (y/n) "
-read ghpage
-if [ "$ghpage" = "y" ]; then
-    git checkout --orphan gh-pages
-    git rm -rf .
-    touch .nojekyll
-    echo $repo_name > index.html
+create_repo() {
+    echo -n "Initialising local repository ..."
+    git init > /dev/null 2>&1
+    git config --local user.name kallimachos
     git add .
-    git commit -a -m "First pages commit"
-    git push -u origin gh-pages > /dev/null 2>&1
-    echo "gh-page available at http://kallimachos.github.io/$repo_name."
-    git checkout master
+    git commit -q -a -m "Initial commit"
+    echo " done."
+
+    echo -n "Creating Github repository '$repo_name' ..."
+    curl -u "$username:$token" https://api.github.com/user/repos -d \
+    '{"name":"'$repo_name'"}' > /dev/null 2>&1
+    echo " done."
+
+    echo -n "Pushing local code to remote ..."
+    git remote add origin git@github.com:$username/$repo_name.git > /dev/null 2>&1
+    git push -u origin master > /dev/null 2>&1
+    echo " done."
+}
+
+create_ghpage() {
+    echo -n "Create gh-page? (y/n) "
+    read ghpage
+    if [ "$ghpage" = "y" ]; then
+        git checkout --orphan gh-pages
+        git rm -rf .
+        touch .nojekyll
+        echo $repo_name > index.html
+        git add .
+        git commit -a -m "First pages commit"
+        git push -u origin gh-pages > /dev/null 2>&1
+        echo "gh-page available at http://kallimachos.github.io/$repo_name."
+        git checkout master
+    else
+        echo "No gh-page created."
+    fi
+}
+
+# Handle arguments
+
+if test $# -gt 0; then
+  display_help
 else
-    echo "No gh-page created."
+  repo_name
+  git_config
+  template
+  create_repo
+  create_ghpage
+  echo "Please update README.rst, setup.py, and enable linkcheck and Travis builds if required."
 fi
-echo "Please update README.rst and enable Travis builds if required."
