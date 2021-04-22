72c93b0b414e6f5e65bdeef48572e1f6cd44e43f
kallimachos <kallimachos@gmail.com>
2021-04-22 12:19:31 +1000

    Update repocheck to handle main branch

--- repocheck.sh
+++ repocheck.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # Print 'git status -s -b' of repos in the listed directories if they have
-# uncommitted changes or if they are not on the master branch.
+# uncommitted changes or if they are not on the master or main branch.
 
 repos=(mongodb mongodb/docs code code/python scripts)
+branches=("## master...origin/master" "## main...origin/main")
 
 echo
 for item in ${repos[@]}; do
@@ -12,7 +13,9 @@ for item in ${repos[@]}; do
         if test -d $dir && test -e $dir/.git; then
             cd $dir && echo $dir
             branch=$(git status -s -b)
-            if ! [ "$branch" = "## master...origin/master" ]; then
+            if [[ " ${branches[*]} " == *"$branch"* ]]; then
+                true
+            else
                 git status -s -b
             fi
         fi
