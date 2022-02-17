932f2617b1793f2fdfc4d7c62c6894c56406ba32
kallimachos <kallimachos@gmail.com>
2022-02-17 10:41:11 +1000

    add staging logs

--- logs.sh
+++ logs.sh
@@ -4,7 +4,8 @@
 
 div='======================'
 
-echo
+kubectl config use-context api.staging.corp.mongodb.com
+echo -e '\nSTAGING'
 echo $div
 echo 'pods'
 echo $div
@@ -15,7 +16,29 @@ echo
 echo $div
 echo 'kbdash'
 echo $div
-kubectl logs --tail=20 -l release=kbdash
+kubectl logs --tail=10 -l release=kbdash
+echo
+
+echo
+echo $div
+echo 'kbtools'
+echo $div
+kubectl logs --tail=10 -l release=kbtools
+echo
+
+kubectl config use-context api.prod.corp.mongodb.com
+echo -e '\nPROD'
+echo $div
+echo 'pods'
+echo $div
+kubectl get pods
+echo
+
+echo
+echo $div
+echo 'kbdash'
+echo $div
+kubectl logs --tail=10 -l release=kbdash
 echo
 
 echo
