59905312d845b1ea50cf9f337e4d77318252e7c1
kallimachos <kallimachos@gmail.com>
2022-08-17 17:22:35 +1000

    Update mup.py

--- mup.sh
+++ mup.sh
@@ -4,21 +4,15 @@
 # then push result to origin if required.
 
 for dir in ~/mongodb/*; do
-    if [[ "$dir" == *"kb"* ]]; then
+    if test -e $dir/.git; then
         cd $dir
         git pull
-    elif test -d $dir && test -e $dir/.git; then
-        cd $dir
-        git fetch upstream
-        git merge upstream/master
-        git push origin master
     fi
 done
-for dir in ~/mongodb/docs/*; do
-    if test -d $dir && test -e $dir/.git; then
-        cd $dir
-        git fetch upstream
-        git merge upstream/master
-        git push origin master
-    fi
-done
+
+# if test -d $dir && test -e $dir/.git; then
+#     cd $dir
+#     git fetch upstream
+#     git merge upstream/master
+#     git push origin master
+# fi
